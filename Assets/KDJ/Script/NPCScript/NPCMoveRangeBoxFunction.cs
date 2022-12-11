05eca5fcfb0961882b3650ae91bffbd239fc3e92
kdj926 <dongju926@gmail.com>
2022-12-11 22:40:16 +0900

    김동주
    22.12.11

---
+++ Assets/KDJ/Script/NPCScript/NPCMoveRangeBoxFunction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMoveRangeBoxFunction : MonoBehaviour
+{
+    private GameObject npcObject;
+    private void Awake()
+    {
+        //npcObject = 
+    }
+
+}
