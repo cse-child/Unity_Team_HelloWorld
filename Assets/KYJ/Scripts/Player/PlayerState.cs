3378e5e05a07be1feaac3b02b24717040f5e6c9c
minong9 <93637301+minong9@users.noreply.github.com>
2022-12-11 22:05:43 +0900

    TotalMapScene
    마을, 농장, 필드, 던전 맵 생성

---
+++ Assets/KYJ/Scripts/Player/PlayerState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public int hp = 100;
+    public int mp = 100;
+    public int money = 10000;
+}
