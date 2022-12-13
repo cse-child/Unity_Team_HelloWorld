20e53de8154d9f686bd49a481f465fb6352d0e6a
cse-child <99868973+cse-child@users.noreply.github.com>
2022-12-13 16:13:58 +0900

    # 플레이어 중력 적용
    - 사라졌던 중력을 되찾았다!

--- Assets/KYJ/Scripts/Player/PlayerState.cs
+++ Assets/KYJ/Scripts/Player/PlayerState.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    public int hp = 100;
-    public int mp = 100;
-    public int money = 10000;
+    const int maxHp = 100;
+    const int maxMp = 100;
+    const float maxExp = 100.0f;
+
+    public int curHp = 100;
+    public int curMp = 100;
+    public float curExp = 0.0f;
+    public int gold = 10000;
 }
