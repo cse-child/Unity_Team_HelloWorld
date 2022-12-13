5534483c790d7b8d7848a03ca0e3e7bc7e309c52
cse-child <99868973+cse-child@users.noreply.github.com>
2022-12-14 02:05:20 +0900

    # 플레이어 에셋 변경, 무기 장착/해제 추가

--- Assets/KYJ/Scripts/Player/PlayerControl.cs
+++ Assets/KYJ/Scripts/Player/PlayerControl.cs
@@ -6,6 +6,12 @@ public class PlayerControl : MonoBehaviour
 {
     Animator animator;
 
+    const int MAX_WEAPON_COUNT = 1;
+
+    public GameObject weaponSocket;
+
+    private int curWeaponState;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -13,14 +19,33 @@ public class PlayerControl : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F1))
+        curWeaponState = animator.GetInteger("WeaponState");
+
+        if (Input.GetKeyDown(KeyCode.F1))
             animator.SetTrigger("Death");
         if (Input.GetKeyDown(KeyCode.F2))
-            animator.SetTrigger("PickUp");
+            animator.SetTrigger("Looting");
         // 마우스 왼쪽 버튼
         if(Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("Attack");
+        }
+
+        // 무기 장착/해제/변경
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (curWeaponState == MAX_WEAPON_COUNT)
+            {
+                animator.SetInteger("WeaponState", 0); // 맨손
+                if(weaponSocket)
+                    weaponSocket.SetActive(false);
+            }
+            else
+            {
+                animator.SetInteger("WeaponState", ++curWeaponState); // 무기장착
+                if (weaponSocket)
+                    weaponSocket.SetActive(true);
+            }
         }
     }
 }
--- Assets/KYJ/Scripts/Player/PlayerState.cs
+++ Assets/KYJ/Scripts/Player/PlayerState.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    const int maxHp = 100;
-    const int maxMp = 100;
-    const float maxExp = 100.0f;
+    const int MAX_HP = 100;
+    const int MAX_MP = 100;
+    const float MAX_EXP = 100.0f;
 
     public int curHp = 100;
     public int curMp = 100;
