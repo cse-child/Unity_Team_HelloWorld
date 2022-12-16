838bbc9c7cbd3a1cfdc02019cb49c49932d38a14
cse-child <99868973+cse-child@users.noreply.github.com>
2022-12-17 01:54:26 +0900

    # 양손검 완성, 마법사 prefab 추가
    - 양손검 애니메이션 완성 (스킬제외)
    - 공격하거나 아이템 줍기 등 플레이어 움직임 예외처리
    - 마법사 프리팹 만들기+추가
    - 캐릭터 파츠(커마) 구현 방법 모색

---
+++ Assets/KYJ/Scripts/Player/PlayerPartsControl.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPartsControl : MonoBehaviour
+{
+    public GameObject hat;
+
+    private void Awake()
+    {
+        hat.transform.GetChild(1).gameObject.SetActive(true);
+    }
+}
