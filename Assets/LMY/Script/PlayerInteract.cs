bddcfc20d56a365bf50b1025c00f115aafdd18bf
minong9 <93637301+minong9@users.noreply.github.com>
2022-12-21 15:45:26 +0900

    상호작용 스크립트 만드는 중

---
+++ Assets/LMY/Script/PlayerInteract.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            float interactRange = 2.0f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent(out NPCInteractable npcInteractable))
+                {
+                    npcInteractable.Interact(transform);
+                }
+            }
+        }
+        
+    }
+}
