096d07a16e6af33736b69ce39c2b1b20b3752967
minong9 <93637301+minong9@users.noreply.github.com>
2022-12-21 21:14:18 +0900

    상호작용 스크립트 추가중
    문, NPC까지 완료

--- Assets/LMY/Script/PlayerInteract.cs
+++ Assets/LMY/Script/PlayerInteract.cs
@@ -8,20 +8,45 @@ public class PlayerInteract : MonoBehaviour
     {     
         if(Input.GetKeyDown(KeyCode.F))
         {
-            float interactRange = 2.0f;
-            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
-            foreach (Collider collider in colliderArray)
+            IInteractable interactable = GetInteractableObject();
+            if(interactable != null)
             {
-                if (collider.TryGetComponent(out NPCInteractable npcInteractable))
-                {
-                    npcInteractable.Interact(transform);
-                }
+                interactable.Interact(transform);
             }
         }   
     }
 
-    //public NPCInteractable GetInteractableObject()
-    //{
+    public IInteractable GetInteractableObject()
+    {
+        List<IInteractable> interactableList = new List<IInteractable>();
+        float interactRange = 2.0f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+        foreach (Collider collider in colliderArray)
+        {
+            if (collider.TryGetComponent(out IInteractable interactable))
+            {
+                interactableList.Add(interactable);
+            }
+        }
 
-    //}
+        IInteractable closestInteractable = null;
+        foreach (IInteractable interactable in interactableList)
+        {
+            if(closestInteractable == null)
+            {
+                closestInteractable = interactable;
+            }
+            else
+            {
+                if(Vector3.Distance(transform.position, interactable.GetTransform().position) < 
+                    Vector3.Distance(transform.position, closestInteractable.GetTransform().position))
+                {
+                    //가까운쪽으로 상호작용
+                    closestInteractable = interactable;
+                }
+            }
+        }
+
+        return closestInteractable;
+    }
 }
