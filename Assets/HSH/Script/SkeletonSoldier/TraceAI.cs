740a157f37de9505910bdc08ce83ac8240c3d52d
JulyDeveloper <88377540+JulyDeveloper@users.noreply.github.com>
2022-12-26 20:01:24 +0900

    몬스터 배회 수정 보스및 스킬 생성
    몬스터 배회 수정 보스및 스킬 생성
    
    2022.12.26

--- Assets/HSH/Script/SkeletonSoldier/TraceAI.cs
+++ Assets/HSH/Script/SkeletonSoldier/TraceAI.cs
@@ -20,8 +20,7 @@ public class TraceAI : MonoBehaviour
     //private float maxHp = 100.0f;
     private float curHp = 100.0f;
 
-    private bool isWander = false;
-    public Transform target;
+    private GameObject target;
     private Animator animator;
 
     public GameObject itemPrefab;
@@ -36,6 +35,7 @@ public class TraceAI : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         closeAtk = GetComponent<MonsterCloseAttack>();
+        target = GameObject.FindGameObjectWithTag("Player");
     }
     private void Start() // 여러번 실행될 수 있으므로 할당 x
     {
@@ -49,18 +49,11 @@ public class TraceAI : MonoBehaviour
             //animator.SetTrigger("trigDie");
             //animator.SetBool("isDie", true);
             //curState = State.DEAD;
-
-            Vector3 targetPosition = new Vector3(CalculateWanderPosition().x, 0, CalculateWanderPosition().z);
-            Vector3 rotDir = transform.position - targetPosition;
-            //transform.rotation = Quaternion.LookRotation(rotDir);
-            Quaternion rotation = Quaternion.LookRotation(rotDir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.01f);
-            animator.SetTrigger("trigExploration");
-            isWander = false;
         }
         //rigidbody.velocity = Vector3.zero; // 물리적 가속도를 0으로 만드는 코드 이때 rigidbody의 Freeze Position은 해제상태로
         SetAction();
         Hurt();
+        RotateMove();
     }
 
     private void OnDrawGizmos()
@@ -83,7 +76,7 @@ public class TraceAI : MonoBehaviour
                 continue;
             }
 
-            float distance = Vector3.Distance(transform.position, target.position);
+            float distance = Vector3.Distance(transform.position, target.transform.position);
             if (distance < attackRange)
                 curState = State.ATTACK;
             else if (distance < traceRange)
@@ -117,9 +110,12 @@ public class TraceAI : MonoBehaviour
     {
         if (animator.GetBool("isDie")) return;
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Skeleton@Attack01")) return;
-        if (animator.GetBool("isAttack") == true) return;
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Skeleton@Idle01_Action01")) return;
+
+        animator.SetBool("isAttack", false);
         animator.SetBool("isTrace", true);
-        Vector3 direction = target.position - transform.position;
+
+        Vector3 direction = target.transform.position - transform.position;
         transform.rotation = Quaternion.LookRotation(direction);
 
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.Self);
@@ -130,44 +126,34 @@ public class TraceAI : MonoBehaviour
     //    animator.SetBool("isAttack", true);
     //    animator.SetBool("isTrace", false);
     //}
-    //private void EndAttack()
-    //{
-    //    animator.SetBool("isAttack", false);
-    //}
+    private void EndAttack()
+    {
+        animator.SetBool("isAttack", false);
+    }
     private void Idle()
     {
         animator.SetBool("isAttack", false);
         animator.SetBool("isTrace", false);
-        RotateMove();
+ 
     }
     private void RotateMove()
     {
+        if(animator.GetBool("isTrace")) return;
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Skeleton@Idle01_Action01"))
         {
-            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.902f)
             {
-                if(isWander)
-                {
-                    Vector3 targetPosition = new Vector3(CalculateWanderPosition().x, 0, CalculateWanderPosition().z);
-                    Vector3 rotDir = transform.position - targetPosition;
-                    //transform.rotation = Quaternion.LookRotation(rotDir);
-                    Quaternion rotation = Quaternion.LookRotation(rotDir);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.01f);
-                    animator.SetTrigger("trigExploration");
-                    isWander = false;
-                }
+                Vector3 targetPosition = new Vector3(CalculateWanderPosition().x, 0, CalculateWanderPosition().z);
+                Vector3 rotDir = targetPosition - transform.position;
+                transform.rotation = Quaternion.LookRotation(rotDir);
+                //천천히 변경
+                //Quaternion rotation = Quaternion.LookRotation(rotDir);
+                //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.01f);
+                animator.SetTrigger("trigExploration");
             }
         }
     }
-    private Quaternion Wander()
-    {
-        isWander = true;
-        Vector3 targetPosition = new Vector3(CalculateWanderPosition().x, 0, CalculateWanderPosition().z);
-        Vector3 rotDir = transform.position - targetPosition;
-        //transform.rotation = Quaternion.LookRotation(rotDir);
-        Quaternion rotation = Quaternion.LookRotation(rotDir);
-        return rotation;
-    }
+
     private Vector3 CalculateWanderPosition()
     {
         float wanderRadius = 10.0f; // 현재 위치를 원점으로 하는 원의 반지름
