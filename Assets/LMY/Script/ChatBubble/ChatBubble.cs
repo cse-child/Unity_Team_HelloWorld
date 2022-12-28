bd531e0a3cfdd6b82124771407d7551431390983
minong9 <93637301+minong9@users.noreply.github.com>
2022-12-28 21:08:27 +0900

    필요없는 폴더 지움

--- Assets/LMY/Script/ChatBubble/ChatBubble.cs
+++ Assets/LMY/Script/ChatBubble/ChatBubble.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ChatBubble : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //public static void Create(Transform parent, Vector3 localPosition, IconType iconType, string text)
+    //{
+    //    Transform chatBubbleTransform = Instantiate(target, parent);
+    //    chatBubbleTransform.localPosition = localPosition;
+
+    //    chatBubbleTransform.GetComponent<ChatBubble>().Setup(iconType, text);
+
+    //    Destroy(chatBubbleTransform.gameObject, 4.0f);
+    //}
+
+    public enum IconType
     {
-        
+        Happy,
+        Neutral,
+        Angry,
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Sprite happyIconSprite;
+    [SerializeField] private Sprite neutralIconSprite;
+    [SerializeField] private Sprite angryIconSprite;
+
+    private SpriteRenderer backgroundSpriteRenderer;
+    private SpriteRenderer iconSpriteRenderer;
+    private TextMeshPro textMeshPro;
+
+    private void Awake()
     {
-        
+        backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
+        iconSpriteRenderer = transform.Find("Icon").GetComponent<SpriteRenderer>();
+        textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
+    }
+
+    private void Start()
+    {
+        Setup(IconType.Neutral, "Hello Hero! May I Help You?");
+    }
+
+    private void Setup(IconType iconType, string text)
+    {
+        textMeshPro.SetText(text);
+        textMeshPro.ForceMeshUpdate();
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+
+        Vector2 padding = new Vector2(7.0f, 3.0f);
+        backgroundSpriteRenderer.size = textSize + padding;
+
+        Vector3 offset = new Vector2(-3.0f, 0.0f);
+        backgroundSpriteRenderer.transform.localPosition =
+            new Vector3(backgroundSpriteRenderer.size.x / 2.0f, 0.0f) + offset;
+
+        iconSpriteRenderer.sprite = GetIconSprite(iconType);
+    }
+
+    private Sprite GetIconSprite(IconType iconType)
+    {
+        switch (iconType)
+        {
+            default:
+            case IconType.Happy: return happyIconSprite;
+            case IconType.Neutral: return neutralIconSprite;
+            case IconType.Angry: return angryIconSprite;
+
+        }
     }
 }
