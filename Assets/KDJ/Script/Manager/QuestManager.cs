7ecbc34b40aea0e4b2c556f73946342728ca5965
kdj926 <62312542+kdj926@users.noreply.github.com>
2023-01-01 21:04:56 +0900

    김동주 NPC 퀘스트
    23.01.01

--- Assets/KDJ/Script/Manager/QuestManager.cs
+++ Assets/KDJ/Script/Manager/QuestManager.cs
@@ -19,10 +19,50 @@ public class QuestManager : MonoBehaviour
         }
     }
 
-    public List<List<int>> questData = new List<List<int>>();
-
-    public void LoadQuestData()
+    public struct QuestData
     {
+        public string questCode;
+        public string questName;
+        public string questType;
+        public string questGoal;
+        public string questDetail;
+        public string questAchivement;
+        public string questLevelRequire;
+    }
 
+    List<QuestData> quests = new List<QuestData>();
+
+    public void Awake()
+    {
+        LoadQuestData();
+    }
+
+    private void LoadQuestData()
+    {
+        string temp = System.IO.File.ReadAllText("Assets/KDJ/TextData/QuestData.csv");
+        temp = temp.Replace("\r\n", "\n");
+        string[] row = temp.Split("\n");
+        for(int i = 1; i < row.Length; i++)
+        {
+            string[] col = row[i].Split(",");
+            //string[] questValue = col[2].Split("_");
+            //string[] questAchieveValue = col[5].Split("_");
+
+            QuestData questData;
+            questData.questCode = col[0];
+            questData.questName = col[1];
+            questData.questType = col[2];
+            questData.questGoal = col[3];
+            questData.questDetail = col[4];
+            questData.questAchivement = col[5];
+            questData.questLevelRequire = col[6];
+
+            quests.Add(questData);
+        }
+    }
+
+    public QuestData GetQuestData(int num)
+    {
+        return quests[num - 1];
     }
 }
