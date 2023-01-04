2d2627798484c9e1c77e3f7fee675b782267119c
kdj926 <62312542+kdj926@users.noreply.github.com>
2023-01-04 21:01:12 +0900

    김동주 NPC 퀘스트
    23.01.04

--- Assets/KDJ/Script/NPCScript/NPCFunction.cs
+++ Assets/KDJ/Script/NPCScript/NPCFunction.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class NPCFunction : MonoBehaviour
@@ -11,11 +13,13 @@ public class NPCFunction : MonoBehaviour
     private void Awake()
     {
         npcRotation = GetComponent<NPCRotation>();
+        this.AddComponent<QuestFunction>();
     }
 
     private void Update()
     {
         IsNPCRotation();
+        GiveQuestToPlayer();
     }
     private void IsNPCRotation()
     {
@@ -24,6 +28,7 @@ public class NPCFunction : MonoBehaviour
         else
             npcRotation.SetNPCRotation(false);
     }
+
     public void SetIsTalkingPlayerToNPC(bool input)
     {
         isTalkingPlayerToNPC = input;
