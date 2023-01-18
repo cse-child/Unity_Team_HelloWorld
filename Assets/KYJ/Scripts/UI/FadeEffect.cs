aa552b50e60407566565fd05736df67f2add76b6
cse-child <99868973+cse-child@users.noreply.github.com>
2023-01-18 21:39:33 +0900

    # 부활할때 Fade 효과 추가

--- Assets/KYJ/Scripts/UI/FadeEffect.cs
+++ Assets/KYJ/Scripts/UI/FadeEffect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public enum FadeState { FadeIn = 0, FadeOut, FadeInOut, FadeLoop }
@@ -11,13 +12,21 @@ public class FadeEffect : MonoBehaviour
     [SerializeField]
     [Range(0.01f, 10f)]
     private float fadeTime; // 10이면 1초 (값이 클수록 빠름)
-    private Image image;
     private FadeState fadeState;
+
+    //public GameObject blood;
+    //public GameObject black;
+    public Image bloodScreen;
+    public Image blackImage;
+
+    private Coroutine bloodCoroutine;
+    private Coroutine blackCoroutine;
+
+    //public Image image;
 
     private void Awake()
     {
-        image = GetComponent<Image>();
-
+        //image = GetComponent<Image>();
         // Fade In. 배경의 알파값이 1에서 0으로
         //StartCoroutine(Fade(1, 0));
 
@@ -27,39 +36,73 @@ public class FadeEffect : MonoBehaviour
         //OnFade(FadeState.FadeLoop);
     }
 
-    public void OnFade(FadeState state)
+    public void OnFade(string key, FadeState state)
     {
         fadeState = state;
 
-        switch(fadeState)
+        if (key == "blood")
         {
-            case FadeState.FadeIn:
-                StartCoroutine(Fade(1, 0));
-                break;
-            case FadeState.FadeOut:
-                StartCoroutine(Fade(0, 1));
-                break;
-            case FadeState.FadeInOut:
-            case FadeState.FadeLoop:
-                StartCoroutine(FadeInOut());
-                break;
+            switch (fadeState)
+            {
+                case FadeState.FadeIn:
+                    bloodCoroutine = StartCoroutine(BloodFade(1, 0));
+                    break;
+                case FadeState.FadeOut:
+                    bloodCoroutine = StartCoroutine(BloodFade(0, 1));
+                    break;
+                case FadeState.FadeInOut:
+                case FadeState.FadeLoop:
+                    StartCoroutine(FadeInOut(key));
+                    break;
+            }
+        }
+        else if (key == "black")
+        {
+            switch (fadeState)
+            {
+                case FadeState.FadeIn:
+                    blackCoroutine = StartCoroutine(BlackFade(1, 0));
+                    break;
+                case FadeState.FadeOut:
+                    blackCoroutine = StartCoroutine(BlackFade(0, 1));
+                    break;
+                case FadeState.FadeInOut:
+                case FadeState.FadeLoop:
+                    StartCoroutine(FadeInOut(key));
+                    break;
+            }
         }
     }
 
-    private IEnumerator FadeInOut()
+    private IEnumerator FadeInOut(string key)
     {
-        while(true)
+        if (key == "blood")
         {
-            yield return StartCoroutine(Fade(1, 0));
-            
-            yield return StartCoroutine(Fade(0, 1));
+            while (true)
+            {
+                yield return bloodCoroutine = StartCoroutine(BloodFade(1, 0));
 
-            if (fadeState == FadeState.FadeInOut)
-                break;
+                yield return bloodCoroutine = StartCoroutine(BloodFade(0, 1));
+
+                if (fadeState == FadeState.FadeInOut)
+                    break;
+            }
+        }
+        else if (key == "black")
+        {
+            while (true)
+            {
+                yield return blackCoroutine = StartCoroutine(BlackFade(1, 0));
+
+                yield return blackCoroutine = StartCoroutine(BlackFade(0, 1));
+
+                if (fadeState == FadeState.FadeInOut)
+                    break;
+            }
         }
     }
 
-    private IEnumerator Fade(float start, float end)
+    private IEnumerator BloodFade(float start, float end)
     {
         float currentTime = 0.0f;
         float percent = 0.0f;
@@ -72,19 +115,60 @@ public class FadeEffect : MonoBehaviour
             percent = currentTime / fadeTime;
 
             // 알파값을 start부터 end까지 fadeTime 시간 동안 변화시킨다
-            Color color = image.color;
+            Color color = bloodScreen.color;
             color.a = Mathf.Lerp(start, end, percent);
-            image.color = color;
+            bloodScreen.color = color;
+
+            print(bloodScreen.name);
 
             yield return null;
         }
     }
 
-    public void StopFade()
+    private IEnumerator BlackFade(float start, float end)
     {
-        StopAllCoroutines();
-        Color color = image.color;
-        color.a = 0.0f;
-        image.color = color;
+        float currentTime = 0.0f;
+        float percent = 0.0f;
+
+        while (percent < 1)
+        {
+            // fadeTime으로 나누어서 fadeTime 시간 동안
+            // percent 값이 0에서 1로 증가하도록 함
+            currentTime += Time.deltaTime;
+            percent = currentTime / fadeTime;
+
+            // 알파값을 start부터 end까지 fadeTime 시간 동안 변화시킨다
+            Color color = blackImage.color;
+            color.a = Mathf.Lerp(start, end, percent);
+            blackImage.color = color;
+
+            print(blackImage.name);
+
+            yield return null;
+        }
+    }
+
+    public void StopFade(string key)
+    {
+        if (key == "blood")
+        {
+            //StopCoroutine("BloodFade");
+            StopCoroutine(bloodCoroutine);
+            Color color = bloodScreen.color;
+            color.a = 0.0f;
+            bloodScreen.color = color;
+        }
+        else if (key == "black")
+        {
+            //StopCoroutine("BlackFade");
+            StopCoroutine(blackCoroutine);
+            Color color = blackImage.color;
+            color.a = 0.0f;
+            blackImage.color = color;
+        }
+
+        //StopAllCoroutines();
+        
+        
     }
 }
